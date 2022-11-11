8ba8224ffd917a9a4c20dda4daaeb73f7d2831f0
smartvain <zlc.blue15210@gmail.com>
2022-11-11 13:06:03 +0900

    add: css style to HomeView component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,63 @@
 
 <template>
   <main>
-    <div>hello</div>
+    <div class="btn-wrapper">
+      <button class="increment-button btn" style="margin-right: 8px" @click="store.increment()">Increment</button>
+      <button class="decrement-button btn" @click="store.decrement()">Decrement</button>
+    </div>
+    <div class="counter-wrapper">
+      <p>Count is {{ store.count }}</p>
+      <p>Double is {{ store.doubleCount }}</p>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.btn-wrapper {
+  margin-bottom: 8px;
+}
+
+.btn {
+  padding: 12px;
+  font-size: 14px;
+  border: none;
+  transition: 0.4s ease;
+  border-radius: 4px;
+}
+
+.btn:hover {
+  cursor: pointer;
+}
+
+.increment-button {
+  background-color: var(--increment-bgc-color);
+  color: white;
+}
+
+.increment-button:hover {
+  background-color: var(--increment-bgc-color-hover);
+  color: var(--increment-bgc-color);
+}
+
+.decrement-button {
+  background-color: var(--decrement-bgc-color);
+  color: white;
+}
+
+.decrement-button:hover {
+  background-color: var(--decrement-bgc-color-hover);
+  color: var(--decrement-bgc-color);
+}
+
+.counter-wrapper {
+  display: flex;
+  gap: 16px;
+}
+
+.counter-wrapper p {
+  padding: 14px;
+  border: none;
+  border-radius: 4px;
+  background-color: #f1f1f1;
+}
+</style>
