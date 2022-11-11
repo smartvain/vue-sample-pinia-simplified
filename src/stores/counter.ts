3922966129362d9ee3bce8ecfa082ecef9442dae
smartvain <zlc.blue15210@gmail.com>
2022-11-11 12:28:50 +0900

    add: counter.ts to stores

---
+++ src/stores/counter.ts
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('counter', {
+  state: () => {
+    return { count: 0 }
+  },
+  actions: {
+    increment(val = 1) {
+      this.count += val
+    },
+    async waitAndAdd() {
+      setTimeout(() => {
+        this.count++
+      }, 2000)
+    }
+  },
+  getters: {
+    doubleCount: (state) => state.count * 2,
+  }
+})
